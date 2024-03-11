e6b08c60f03ed6662c2421e96d97d8c0e01f8980
Rafi Siddique <rafisiddique67@gmail.com>
2024-03-11 20:58:14 +0600

    Add project files.

---
+++ Personal_Expense_Tracking_System_Web_Api/Data/ApplicationDB.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Personal_Expense_Tracking_System_Web_Api.Data
+{
+    public class ApplicationDB : DbContext
+    {
+        public ApplicationDB(DbContextOptions options) : base(options) { }
+    }
+}
